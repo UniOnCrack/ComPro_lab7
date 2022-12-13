66860aa3b4ab4c7e33689b40f1aaf3201fdd7448
uni <ni.nitid@gmail.com>
2022-12-13 16:34:35 +0700

    Lab7_1 Finished

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -23,19 +23,28 @@ string func2(string x){
 	return y;	
 }
 
-string func3(string x){
-	int i = 0, L = x.size();
-	string y = "";
-	while(i < L){
-		y += tolower(x[i]);
-		i++;
-	}
-	return y;	
-}
-
 int main(){
-    cout >> "Input text: "
-    cout >> "Reversed text: "
-    cout >> "Palindrome: "
+	string A, B, C, D;
+
+	cout << "Input text: ";
+	cin >> A;
+	
+	B = func1(A);
+	C = func2(B);
+	D = func2(A);
+
+
+	cout << "Reversed text: " << B << endl;
+
+
+	cout << "Palindrome: ";
+
+	if(C == D){
+		cout << "Yes";
+	}else{
+		cout << "No";
+	}
+
+
     return 0;
 }
